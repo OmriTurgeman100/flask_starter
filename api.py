a5a77d45990228d349fd44cee4b68a533feba562
omri <omrigamer100@gmail.com>
2025-04-20 15:53:10 +0300

    test

--- api.py
+++ api.py
@@ -3,12 +3,13 @@ from flask_cors import CORS
 from psycopg2.extras import RealDictCursor
 from database import get_database_connection
 
+
 api = Flask(__name__)
 
 CORS(api)
 
 @api.route("/api/v1/todos", methods=["GET"])
-def get_nodes():
+def get_todos():
     try:
         postgres = get_database_connection()
         cursor = postgres.cursor(cursor_factory=RealDictCursor) 
@@ -24,6 +25,129 @@ def get_nodes():
     finally:
         cursor.close()
         postgres.close()
+
+
+@api.route("/api/v1/todos/<id>", methods=["GET"])
+def get_specific_todo(id):
+    try:
+        postgres = get_database_connection()
+        cursor = postgres.cursor(cursor_factory=RealDictCursor) 
+
+        print(id)
+
+        cursor.execute("select * from todos where id = %s", (id,))
+        response = cursor.fetchall()  
+
+        return jsonify(response), 200
+        
+    except Exception as e:
+        print(e)
+        return jsonify({"error": str(e)}), 500 
+    finally:
+        cursor.close()
+        postgres.close()
+
+
+@api.route("/api/v1/todos/<id>", methods=["DELETE"])
+def delete_todo(id):
+    try:
+        postgres = get_database_connection()
+        cursor = postgres.cursor(cursor_factory=RealDictCursor) 
+
+        cursor.execute("delete from todos where id = %s", (id))
+
+        postgres.commit()
+
+        # response = cursor.fetchall()  
+
+        return jsonify("todo deleted 200"), 200
+        
+    except Exception as e:
+        print(e)
+        return jsonify({"error": str(e)}), 500 
+    finally:
+        cursor.close()
+        postgres.close()
+
+
+
+@api.route("/api/v1/todos/post", methods=["POST"])
+def post_todo():
+    try:
+        postgres = get_database_connection()
+        cursor = postgres.cursor(cursor_factory=RealDictCursor) 
+
+        data = request.json
+
+        title = data.get('title')
+        description = data.get('description')
+
+        # print(title, description)
+
+        cursor.execute("insert into todos (title, description) values (%s, %s) returning *", (title, description))
+
+        postgres.commit()
+
+        response = cursor.fetchall()  
+
+        heavy_work(title)
+
+        return jsonify(response), 200
+        
+    except Exception as e:
+        print(e)
+        return jsonify({"error": str(e)}), 500 
+    finally:
+        cursor.close()
+        postgres.close()
+
+
+def heavy_work(title):
+    try:
+
+        print(f"specified title is ${title}")
+
+        number = 0
+
+        while True:
+            number += 1
+            print(number)
+
+    except Exception as e:
+        print(e)
+
+
+@api.route("/api/v1/todos/patch/<id>", methods=["PATCH"])
+def patch_todo(id):
+    try:
+        postgres = get_database_connection()
+        cursor = postgres.cursor(cursor_factory=RealDictCursor) 
+
+        data = request.json
+
+        title = data.get('title')
+        # description = data.get('description')
+
+        # print(title, description)
+
+        cursor.execute("update todos set title = %s where id = %s returning *", (title, id))
+
+        postgres.commit()
+
+        response = cursor.fetchall()  
+
+        return jsonify(response), 200
+        
+    except Exception as e:
+        print(e)
+        return jsonify({"error": str(e)}), 500 
+    finally:
+        cursor.close()
+        postgres.close()
+
+
+
+
 
 if __name__ == "__main__":
     api.run(debug=True, host='localhost', port=8000) 
